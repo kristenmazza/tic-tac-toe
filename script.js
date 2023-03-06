e1b3a4ab92bf5e7f1e01d7aa7b6ef04bfd221fcd
kristenmazza <kbmazza@gmail.com>
2023-03-05 17:50:07 -0800

    Work in progress - select first two cells

--- script.js
+++ script.js
@@ -1,39 +1,65 @@
 /* eslint-disable no-plusplus */
 
 const gameboard = (() => {
-    let cellNumber = 1;
-
     const tictactoe = [
         ['', '', ''],
         ['', '', ''],
         ['', '', ''],
     ]
-    
-    function createCell(input) {
-        const board = document.getElementById('gameboard');
-        const cell = document.createElement('div');
-        cell.classList.add('box');
-        board.appendChild(cell);
-        cell.textContent = input;
-        cell.setAttribute('data-id', `${cellNumber}`);
-    }
-    
-    for (let row = 0; row < tictactoe.length; row++) {
-        for (let column = 0; column < tictactoe.length; column++) {
-            const input = tictactoe[row][column]
-            createCell(input);
-            cellNumber++;
+
+    const board = document.getElementById('gameboard');
+
+    function addCells() {
+        let cellNumber = 1;
+        function createCell(input) {
+            const cell = document.createElement('div');
+            cell.classList.add('box');
+            board.appendChild(cell);
+            cell.textContent = input;
+            cell.setAttribute('data-id', `${cellNumber}`);
+        }
+
+        for (let row = 0; row < tictactoe.length; row++) {
+            for (let column = 0; column < tictactoe.length; column++) {
+                const input = tictactoe[row][column];
+                createCell(input);
+                cellNumber++;
+            }
         }
     }
 
-    return { tictactoe };
+    function renderGameboard() {
 
+        while (board.hasChildNodes()) {
+            board.removeChild(board.firstChild);
+        }
+        addCells();
+    
+    }
+    return { tictactoe, renderGameboard };
 })();
 
 const playerFactory = (name, marker) => ({name, marker});
 
 const playerOne = playerFactory('Player 1', 'X');
 const playerTwo = playerFactory('Player 2', 'O');
+
+const gameController = (() => {
+    gameboard.renderGameboard();
+
+    function addMarker(cellId) {
+        if (cellId === '1') {
+            gameboard.tictactoe[0][0] = 'X'; 
+            console.log(gameboard.tictactoe);
+        } else if (cellId === '2') {
+            gameboard.tictactoe[0][1] = '2';
+            console.log(gameboard.tictactoe);
+        }   
+        gameboard.renderGameboard();
+    }
+
+    return { addMarker };
+})();
 
 const cells = document.querySelectorAll('.box');
 cells.forEach(cell => {
@@ -43,19 +69,9 @@ cells.forEach(cell => {
         if (text === '') {
             console.log(e.target.dataset.id);
             cellId = e.target.dataset.id;
+            gameController.addMarker(cellId);
+
         }
-        gameController.addMarker(cellId);
     });
 });
 
-gameController = (() => {
-
-    function addMarker(cellId) {
-        if (cellId === '1') {
-            gameboard.tictactoe[0][0] = 'X'; 
-            console.log(gameboard.tictactoe);
-        }    
-    }
-
-    return { addMarker };
-})();
